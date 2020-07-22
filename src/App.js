94246b9719325a5854df2e80055340bff65502ee
Kike <kike@Kikes-MBP.home>
2020-07-22 18:31:09 +0200

    set up vars, normalize, create two panels, install styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+//IMPORTS
+//-Modules
 import React from 'react'
+import styled from 'styled-components'
+//-Data
+import { colors } from './data/styles/styleVars'
+
+
+
+//STYLES
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+const RoasterPanel = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50%;
+  height: 100%;
+  background: ${colors.pink};
+  h4{
+    font-family: 'AtlasGrotesk-Regular';
+    font-size: 40px;
+    color: ${colors.red};
+    letter-spacing: 0;
+    line-height: 40px;
+    font-weight: 300;
+    text-transform: uppercase;
+  }
+`
+const ShopPanel = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 50%;
+  height: 100%;
+  background: ${colors.white};
+`
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Container>
+      <RoasterPanel>
+        <h4>Roaster</h4>
+      </RoasterPanel>
+      <ShopPanel></ShopPanel>
+    </Container>
   )
 }
 
---
+++ src/data/styles/styleVars.js
@@ -0,0 +1,10 @@
+
+
+const colors = {
+    pink: "#FFEEEF",
+    darkPink: "#EFDBDC",
+    white: "#F7F7F7",
+    red: "#ED3E3F"
+}
+
+export { colors }
